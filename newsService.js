b5652962fd281a4a80b51fabf2f32b6e9f203b1b
Elena Bechmanis <ebechmanis@myseneca.ca>
2022-08-26 09:36:35 -0400

    updated newsService

--- newsService.js
+++ newsService.js
@@ -1,4 +1,8 @@
+const env = require("dotenv")
+env.config()
+
 const Sequelize = require('sequelize');
+
 var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
@@ -38,3 +42,47 @@ module.exports.getAllNews = () => {
         });
 }
 
+module.exports.addNews = (newsData) => {
+    return new Promise((resolve, reject) => {
+        for (const prop in newsData) {
+            if (newsData[prop] === ""){
+                newsData[prop] = null;
+            }
+        }
+        newsData.newsDate = new Date();
+        News.create(newsData).then(() => { resolve()})
+        .catch((err) => reject("Unable to create post"));
+        });    
+}
+
+module.exports.getNewsByMinDate = function(minDateStr){
+    return new Promise((resolve, reject) => {
+        const { gte } = Sequelize.Op;
+        News.findAll({
+            where: {
+                newsDate: {
+                [gte]: new Date(minDateStr)
+            }
+        }
+        }).then((data) => {resolve(data)})
+        .catch((err) => reject("No results returned"));
+        });       
+}
+
+module.exports.getNewsById = (id) => {
+    return new Promise((resolve, reject) => {
+        News.findAll({
+            where: { id: id }
+        }).then((data) => {resolve(data[0])})
+        .catch((err) => reject("No results returned"));
+        });      
+}
+
+module.exports.deleteNewsById = (id) => {
+    return new Promise((resolve, reject) => {
+        News.destroy({
+            where: { id: id }
+        }).then(() => {resolve()})
+        .catch((err) => reject("Error while trying to delete post"));
+        });
+}
